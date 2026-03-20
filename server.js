3e1eef921a2caf1c6a3d45553c284f9ab29304a4
thelasthopefully2-collab <thelasthopefully2@gmail.com>
2026-03-20 22:14:58 +0100

    Update server to listen on all network interfaces

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-  console.log(`🔒 Security middleware enabled`);
+app.listen(PORT, '0.0.0.0', () => {  // 👈 add '0.0.0.0'
+    console.log(`🚀 Server running on port ${PORT}`);
+    console.log(`🔒 Security middleware enabled`);
 });
